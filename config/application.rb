587b70be25835b4982ac185dc472812eda35688a
Hank Beaver <hbeaver@gmail.com>
2021-12-16 12:39:51 -0500

    Autoload lib

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Derailed
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.api_only = false
+
+    config.autoload_paths << File.join(Rails.root,'lib/')
   end
 end
